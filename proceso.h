20a210691ca81ecd0fe7f60011995457bff91bcd
Kenneth Corrales <kcorrales33@gmail.com>
2022-05-17 23:27:14 -0600

    ya crea los procesos con los threads
    Siguiente en hacer=> que los procesos se coloquen en memoria

--- proceso.h
+++ proceso.h
@@ -1,7 +1,7 @@
 #ifndef PROCESO_H
 #define PROCESO_H
 
-typedef struct _proceso{
+typedef struct PROCESO{
     int pid;
     char type; // type of process P = pagination, S = Segmentation
     int time; // time
@@ -9,7 +9,7 @@ typedef struct _proceso{
     int subdivision; // subdivision
 }PROCESO;
 
-PROCESO *create_process(int _pid, char type, int time, int division, int subdivision);
+//PROCESO *create_process(int _pid, char _type, int _time, int _division, int _subdivision);
 
 
 #endif
